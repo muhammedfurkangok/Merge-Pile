6c88e49afc391e69f322468cc32812528dfba0f2
gkkangk@gmail.com <gkkangk@gmail.com>
2024-09-09 18:44:54 +0300

    Player Manager Progress

--- Assets/Scripts/Runtime/Managers/PlayerManager.cs
+++ Assets/Scripts/Runtime/Managers/PlayerManager.cs
@@ -15,14 +15,15 @@ namespace Runtime.Managers
         private Tween moveTween;
 
         #region Self Variables
-        
+
         [SerializeField] private TailAnimator2[] playerTails;
         [SerializeField] private BoingBones boingBones;
-        
+
+        [SerializeField, Range(0.1f, 2f)] private float moveSpeedMultiplier = 1f; 
         public Ease Ease;
 
         #endregion
-     
+
         private void Start()
         {
             ToggleTail(false);
@@ -33,6 +34,7 @@ namespace Runtime.Managers
         {
             boingBones.enabled = enable;
         }
+
         private void ToggleTail(bool enable)
         {
             foreach (var tail in playerTails)
@@ -45,48 +47,46 @@ namespace Runtime.Managers
         {
             moveTween?.Kill();
             ToggleBoing(false);
-          
+
             InputManager.Instance.DisableInput();
             Sequence sequence = DOTween.Sequence();
 
-            float distance = Vector3.Distance(transform.position, position);
-            
-            float moveDuration = Mathf.Clamp(distance * 0.05f, 0.5f,0.9f); 
+            float distance = Vector3.Distance(baseTransform, position);
+            float moveDuration = Mathf.Clamp(distance * 0.05f, 0.3f, 0.5f) / moveSpeedMultiplier; 
 
-           
             sequence.Append(transform.DOMoveX(position.x, moveDuration * 0.4f).SetEase(Ease));
             sequence.AppendCallback(() =>
             {
                 SoundManager.Instance.PlaySound(GameSoundType.Touch);
                 ToggleTail(true);
             });
-            sequence.Append(transform.DOMove(position, moveDuration * 0.6f).SetEase(Ease));
+            sequence.Append(transform.DOMove(position, moveDuration * 0.5f).SetEase(Ease));
             sequence.AppendCallback(() =>
             {
                 item.transform.SetParent(transform);
-                ToggleBoing(true);
-                //anim
+                DOVirtual.DelayedCall(moveDuration * 0.4f, () =>
+                {
+                    ToggleBoing(true);
+                });
             });
-            
-            sequence.Append(transform.DOMove(baseTransform, moveDuration * 0.8f).SetEase(Ease.Linear));
+            sequence.Append(transform.DOMove(baseTransform, moveDuration * 0.5f).SetEase(Ease.Linear));
             sequence.Append(item.transform.DOScale(new Vector3(0.4f, 0.4f, 0.4f), moveDuration * 0.6f).SetEase(Ease));
-            sequence.AppendCallback( () =>
-            {
-                item.transform.SetLayerRecursive( LayerMask.NameToLayer("Slot"));
-            });
+            // sequence.AppendCallback(() =>
+            // {
+            //     item.transform.SetLayerRecursive(LayerMask.NameToLayer("Slot"));
+            // });
             sequence.OnComplete(() =>
             {
                 DOVirtual.DelayedCall(0.015f, () =>
                 {
                     item.OnClick();
                 });
-               
+
                 InputManager.Instance.EnableInput();
                 ToggleTail(false);
             });
 
             moveTween = sequence;
         }
-
     }
 }
